d23efa01c157746a3769b051d677bf437505cf4d
Yurii Dvornichenko <raen98@live.ru>
2022-04-20 22:53:24 +0300

    задание 1

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,31 @@
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+};
+
+refs.startBtn.addEventListener('click', startActiveColors);
+refs.stopBtn.addEventListener('click', stopActiveColors);
+
+const PROMPT_TIME = 1000;
+// let hasColors = false;
+
+function stopActiveColors() {
+  //   hasColors = true;
+  clearInterval(intervalId);
+  refs.startBtn.disabled = false;
+}
+
+function startActiveColors() {
+  //   if (hasColors) {
+  //     return;
+  //   }
+  //   hasColors = true;
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+    refs.startBtn.disabled = true;
+  }, PROMPT_TIME);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
