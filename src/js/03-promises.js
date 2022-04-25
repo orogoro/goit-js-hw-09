8fa7dbcd2767e3bc991f9e31047412ee1ffd6b27
Yurii Dvornichenko <raen98@live.ru>
2022-04-25 18:10:42 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -4,63 +4,8 @@ const refs = {
   delayEl: document.querySelector('input[name="delay"]'),
   stepEl: document.querySelector('input[name="step"]'),
   amountEl: document.querySelector('input[name="amount"]'),
-  // buttonEl: document.querySelector('button[type="submit"]'),
   formEl: document.querySelector('.form'),
 };
-
-// refs.formEl.addEventListener('submit', formSubmit);
-// refs.formEl.addEventListener('input', inputValue);
-
-// function formSubmit() {
-//   // e.preventDefault();
-//   // e.currentTarget.reset();
-//   inputValue();
-// }
-// const selectedForm = [];
-
-// function inputValue(e) {
-//   selectedForm[e.target.name] = e.target.value;
-//   // let { delay, step, amount } = selectedForm;
-
-//   // main(delay, step, amount);
-
-//   Object.entries(selectedForm).forEach(([name, value]) => {
-//     //   selectedForm[name] = value;
-//     refs.formEl.elements[name].value = value;
-//   });
-// }
-
-// // function inputValue() {
-// //   main(refs.delayEl.value, refs.stepEl.value, refs.amountEl.value);
-// // }
-
-// let position = 0;
-
-// function createPromise(position, delay) {
-//   return new Promise((resolve, reject) => {
-//     const shouldResolve = Math.random() > 0.3;
-//     setTimeout(() => {
-//       if (shouldResolve) {
-//         resolve({ position, delay });
-//       } else {
-//         reject({ position, delay });
-//       }
-//     }, delay);
-//   });
-// }
-
-// function main(firstDelay, step, amount) {
-//   for (let i = 1; i <= amount; i += 1) {
-//     const delay = firstDelay + step * (i - 1);
-//     createPromise(i, delay)
-//       .then(({ position, delay }) => {
-//         console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-//       })
-//       .catch(({ position, delay }) => {
-//         console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-//       });
-//   }
-// }
 
 refs.formEl.addEventListener('submit', main);
 refs.formEl.addEventListener('input', inputValue);
